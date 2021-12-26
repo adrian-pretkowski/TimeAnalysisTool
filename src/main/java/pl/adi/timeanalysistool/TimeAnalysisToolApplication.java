b41ab7e0e8bd9ece84f82f6594879f6195589e1a
adrian-pretkowski <adrian.pretkowski@gmail.com>
2021-12-26 15:03:50 +0100

    first commit

---
+++ src/main/java/pl/adi/timeanalysistool/TimeAnalysisToolApplication.java
@@ -0,0 +1,13 @@
+package pl.adi.timeanalysistool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAnalysisToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeAnalysisToolApplication.class, args);
+    }
+
+}
