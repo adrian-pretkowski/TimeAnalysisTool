875277950ff7eec25d23fcddd08444e5ff163f64
Adi <adrian.pretkowski@gmail.com>
2022-02-13 16:11:12 +0100

    Added Post Mapping to receive TestPlan from frontend App (file lines from text file).

--- src/main/java/pl/adi/timeanalysistool/api/TestPlanResource.java
+++ src/main/java/pl/adi/timeanalysistool/api/TestPlanResource.java
@@ -2,6 +2,7 @@ package pl.adi.timeanalysistool.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.adi.timeanalysistool.domain.model.TestPlan;
@@ -30,5 +31,13 @@ public class TestPlanResource {
     @GetMapping("/testplans/{vehicleTyp}/{testLocation}")
     public ResponseEntity<List<TestPlan>> getTestPlansBasedOnVehicleTypAndLocation(@PathVariable String vehicleTyp, @PathVariable String testLocation) {
         return ResponseEntity.ok().body(testPlanService.getTestPlansBasedOnVehicleTypAndTestLocation(vehicleTyp, testLocation));
+    }
+
+    @PostMapping("/testplans/save")
+    public ResponseEntity<TestPlan> saveTestPlanToDatabase(@RequestBody List<String> fileLines) {
+        for (String fileLine : fileLines) {
+            System.out.println(fileLine);
+        }
+        return ResponseEntity.ok().build();
     }
 }
